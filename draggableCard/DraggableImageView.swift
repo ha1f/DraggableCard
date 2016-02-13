b34f8efe90f9b0f50d3ace01f01819ed94adfd48
ha1fha1f <half.ldk.hiz@gmail.com>
2016-02-13 21:58:46 +0900

    とりあえず動作

---
+++ draggableCard/DraggableImageView.swift
@@ -0,0 +1,105 @@
+//
+//  DraggableImageView.swift
+//  draggableCard
+//
+//  Created by 山口智生 on 2016/02/13.
+//  Copyright © 2016年 ha1f. All rights reserved.
+//
+
+import UIKit
+
+class DraggableImageView: UIView {
+    
+}
+
+extension CGRect {
+    func checkContain(point: CGPoint) -> Bool {
+        if (point.x >= self.minX) && (point.x <= self.maxX) && (point.y >= self.minY) && (point.y <= self.maxY) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+extension CGPoint {
+    func vectTo(point: CGPoint) -> (x: CGFloat, y: CGFloat) {
+        return (point.x - self.x, point.y - self.y)
+    }
+    
+    func advancedBy(x: CGFloat, y: CGFloat) -> CGPoint {
+        return CGPoint(x: self.x + x, y: self.y + y)
+    }
+}
+
+class DraggableImageViewField: UIView {
+    
+    var startTouchPoint: CGPoint? = nil
+    var draggingCard: UIView? = nil
+    var startCardPosition: CGPoint? = nil
+    
+    var cards: [UIView] = []
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.clipsToBounds = true
+        self.multipleTouchEnabled = false
+        self.userInteractionEnabled = true
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func addCard(card: UIView) {
+        self.addSubview(card)
+        self.cards.append(card)
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first {
+            startDragging(touch)
+        }
+    }
+    
+    func startDragging(touch: UITouch) {
+        // 上にあるものから順に検査
+        for card in cards.reverse() {
+            if card.frame.contains(touch.locationInView(self)) {
+                startTouchPoint = touch.locationInView(self)
+                draggingCard = card
+                startCardPosition = card.layer.position
+                break
+            }
+        }
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        UIView.animateWithDuration(0.5) {
+            self.draggingCard?.layer.position = self.startCardPosition!
+            self.draggingCard?.transform = CGAffineTransformMakeRotation(0)
+        }
+        startTouchPoint = nil
+        draggingCard = nil
+        startCardPosition = nil
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        if let touch = touches.first {
+            guard self.frame.contains(touch.locationInView(self)) else {
+                self.touchesEnded(Set(), withEvent: nil)
+                return
+            }
+            if let startTouchPoint = startTouchPoint, let card = draggingCard {
+                let touchPoint = touch.locationInView(self)
+                let vect = startTouchPoint.vectTo(touchPoint)
+                card.layer.position = startCardPosition!.advancedBy(vect.x, y: vect.y)
+                draggingCard?.transform = CGAffineTransformMakeRotation(-vect.x/800)
+            } else {
+                startDragging(touch)
+            }
+        }
+    }
+}
--- draggableCard/ViewController.swift
+++ draggableCard/ViewController.swift
@@ -13,6 +13,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let field = DraggableImageViewField(frame: CGRectMake(0, 0, 300, 300))
+        field.backgroundColor = UIColor.yellowColor()
+        self.view.addSubview(field)
+        
+        
+        let imageView = DraggableImageView()
+        imageView.backgroundColor = UIColor.redColor()
+        imageView.frame = CGRectMake(100, 50, 125, 100)
+        imageView.userInteractionEnabled = false
+        field.addCard(imageView)
+        
+        let imageView2 = DraggableImageView()
+        imageView2.backgroundColor = UIColor.redColor()
+        imageView2.frame = CGRectMake(50, 150, 125, 100)
+        imageView2.userInteractionEnabled = false
+        field.addCard(imageView2)
+        
     }
 
     override func didReceiveMemoryWarning() {
